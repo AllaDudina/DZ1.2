18b2a28a6f7cea4073c81f973a1e713395d43e8d
Alla <thedreamtea@gmail.com>
2023-01-15 14:51:45 +1200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 500;
+        int priceMile = 20;
+        int newBonusMiles = price / priceMile;
+
+
+        System.out.println("Вы заработали " + newBonusMiles +  " бонусных миль, Поздравляем!");
+
+    }
+}
